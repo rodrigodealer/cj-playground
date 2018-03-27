087aef90163c42ab7097edfb861da0c27766f09e
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2018-03-27 11:39:55 +0200

    Adding route

--- src/bibliothek_cj/handler.clj
+++ src/bibliothek_cj/handler.clj
@@ -3,8 +3,12 @@
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
+(defn- str-to [num]
+  (apply str (interpose ", " (range 1 (inc num)))))
+
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/count/:to" [to] (str-to (Integer. to)))
   (route/resources "/")
   (route/not-found "Not Found"))
 
